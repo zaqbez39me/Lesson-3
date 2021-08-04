dc2a1dc0c2e432ed9989970d3b5fdfc11b22ca16
Zener_085 <57841939+Zener085@users.noreply.github.com>
2021-08-04 13:05:29 +0300

    Init

---
+++ cmake-build-debug/CMakeFiles/CLionProjects.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CLionProjects.exe"
+  "CLionProjects.exe.manifest"
+  "CLionProjects.lib"
+  "CLionProjects.pdb"
+  "CMakeFiles/CLionProjects.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLionProjects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
